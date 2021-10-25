56d13c9a24ecd4347c6c7acf2ec8d9401a462dfd
Pavel Suprun <pavel.suprun@quantori.com>
2021-10-25 16:43:10 +0300

    transfered methods to appropriate classes

--- CPPapp1/CPPapp1.cpp
+++ CPPapp1/CPPapp1.cpp
@@ -6,17 +6,22 @@
 #include <conio.h>
 #include <Windows.h>
 
-const int mapheight = 15;
-const int mapwidth  = 15;
+
+void set_console_color() { // default color
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
+void set_console_color(int color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
 
 class Map {
 	private:
 		int height;
 		int width;
 		int mapSize;
-		int map[160000];
 		int food = 3;
 	public:
+		int map[160000];
 		void setHeight(int h) {
 			height = h;
 		}
@@ -29,6 +34,45 @@ class Map {
 		}
 		int getWidth() {
 			return width;
+		}
+
+		void setFood(int f) {
+			food = f;
+		}
+		int getFood() {
+			return food;
+		}
+		void generateFood() {
+			int cell = 0;
+			do {
+				cell = (rand() % (height - 2) + 1)*width + rand() % (width - 2) + 1;
+			} while (map[cell] != 0);
+			map[cell] = -2;
+		}
+
+		void printMap() {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
+					int current = map[i*width + j];
+					if (current == 0)
+						std::cout << '_';
+					else if (current == -1)
+						std::cout << 'X';
+					else if (current > 0)
+						std::cout << 'o';
+					else
+						std::cout << 'F';
+					std::cout << " ";
+				}
+				std::cout << '\n';
+			}
+		}
+
+		void updateMap() {
+			for (int i = 1; i < height - 1; i++)
+				for (int j = 1; j < width - 1; j++)
+					if (map[i*width + j]>0)
+						map[i*width + j]--;
 		}
 };
 
@@ -63,152 +107,131 @@ class Snake {
 };
 
 
-bool running;
+class Game {
+	public:
+		Map map;
+		Snake snake;
+		bool running;
 
-
-Move getMove(char key) {
-	switch (key) { //arrows
-	case 'w':
-		return up;
-		break;
-	case 'd':
-		return right;
-		break;
-	case 's':
-		return down;
-		break;
-	case 'a':
-		return left;
-		break;
-	}
-	return no_move;
-}
-void generateFood() {
-	int cell = 0;
-	do {
-		cell = (rand() % (mapheight - 2) + 1)*mapwidth + rand() % (mapwidth - 2) + 1;
-	} while (map[cell] != 0);
-	map[cell] = -2;
-}
-void processMove(Move m) {
-	int dx, dy;
-	switch (m) {
-	case up:
-		dx = 0;
-		dy = -1;
-		break;
-	case right:
-		dx = 1;
-		dy = 0;
-		break;
-	case down:
-		dx = 0;
-		dy = 1;
-		break;
-	case left:
-		dx = -1;
-		dy = 0;
-		break;
-	case no_move:
-		dx = 0;
-		dy = 0;
-		break;
-	}
-
-	int newx = dx + head_x;
-	int newy = dy + head_y;
-
-	int current = newy*mapwidth + newx;
-	if (map[current] == -2) { // food
-		food++;
-		generateFood();
-	}
-	else if (map[current] != 0) { // wall 
-		running = false;
-	}
-	
-	head_x = newx;
-	head_y = newy;
-
-	map[current] = food + 1;
-}
-
-
-void start() {
-	head_x = 3;
-	head_y = 3;
-	move = right;
-	for (int i = 0; i < mapsize; i++)
-		map[i] = 0;
-	for (int i = 0; i < mapheight; i++) {
-		map[i*mapwidth] = -1;
-		map[(i + 1)*mapwidth - 1] = -1;
-	}
-	for (int i = 0; i < mapwidth; i++) {
-		map[i] = -1;
-		map[mapwidth*(mapheight - 1) + i] = -1;
-	}
-	generateFood();
-}
-void printMap() {
-	for (int i = 0; i < mapheight; i++) {
-		for (int j = 0; j < mapwidth; j++) {
-			int current = map[i*mapwidth + j];
-			if (current == 0)
-				std::cout << '_';
-			else if (current == -1)
-				std::cout << 'X';
-			else if (current > 0)
-				std::cout << 'o';
-			else
-				std::cout << 'F';
-			std::cout << " ";
+		Move getMove(char key) {
+			switch (key) { //arrows
+			case 'w':
+				return up;
+				break;
+			case 'd':
+				return right;
+				break;
+			case 's':
+				return down;
+				break;
+			case 'a':
+				return left;
+				break;
+			}
+			return no_move;
 		}
-		std::cout << '\n';
-	}
-}
-void updateMap() {
-	for (int i = 1; i < mapheight - 1; i++)
-		for (int j = 1; j < mapwidth - 1; j++)
-			if (map[i*mapwidth + j]>0)
-				map[i*mapwidth + j]--;
-}
 
+		void processMove(Move m) {
+			int dx, dy;
+			switch (m) {
+			case up:
+				dx = 0;
+				dy = -1;
+				break;
+			case right:
+				dx = 1;
+				dy = 0;
+				break;
+			case down:
+				dx = 0;
+				dy = 1;
+				break;
+			case left:
+				dx = -1;
+				dy = 0;
+				break;
+			case no_move:
+				dx = 0;
+				dy = 0;
+				break;
+			}
 
-void set_console_color() { // default color
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-}
-void set_console_color(int color) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
+			int newx = dx + snake.getHeadX();
+			int newy = dy + snake.getHeadY();
 
+			int current = newy*map.getWidth() + newx;
+			if (map.map[current] == -2) { // food
+				map.setFood(map.getFood()+1);
+				map.generateFood();
+			}
+			else if (map.map[current] != 0) { // wall 
+				running = false;
+			}
+			
+			snake.setHeadX(newx);
+			snake.setHeadY(newy);
+
+			map.map[current] = map.getFood() + 1;
+		}
+
+		void start() {
+			snake.setHeadX(3);
+			snake.setHeadY(3);
+			snake.setMove(right);
+			
+			for (int i = 0; i < map.getHeight()*map.getWidth(); i++)
+				map.map[i] = 0;
+			for (int i = 0; i < map.getHeight(); i++) {
+				map.map[i*map.getWidth()] = -1;
+				map.map[(i + 1)*map.getWidth() - 1] = -1;
+			}
+			for (int i = 0; i < map.getWidth(); i++) {
+				map.map[i] = -1;
+				map.map[map.getWidth()*(map.getHeight() - 1) + i] = -1;
+			}
+			map.generateFood();
+		}
+
+		void runGame() {
+			running = 1;
+			start();
+			map.printMap();
+			while (running) {
+				if (_kbhit()) {
+					char c = _getch();
+					snake.setMove(getMove(c));
+				}
+				processMove(snake.getMove());
+				map.updateMap();
+				system("cls");
+				map.printMap();
+				Sleep(500/map.getFood());
+			}
+			std::cout << "Game over. Your score is: " << map.getFood() << '\n';
+			char c;
+			while (true) {
+				c = _getch();
+				if (c == 'r') {
+					map.setFood(3);
+					runGame();
+				}
+			}
+			std::cin.ignore();
+		}
+};
 
 int main()
 {
-	running = 1;
-	start();
-	printMap();
-	while (running) {
-		if (_kbhit()) {
-			char c = _getch();
-			move = getMove(c);
-		}
-		processMove(move);
-		updateMap();
-		system("cls");
-		printMap();
-		Sleep(500/food);
-	}
-	std::cout << "Game over. Your score is: " << food << '\n';
-	char c;
-	while (true) {
-		c = _getch();
-		if (c == 'r') {
-			food = 3;
-			main();
-		}
-	}
-	std::cin.ignore();
+	Snake snake;
+	Map map;
+	map.setHeight(15);
+	map.setWidth(15);
+
+	Game game;
+	game.map = map;
+	game.snake = snake;
+
+	game.runGame();	
 }
 
 
